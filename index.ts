f6a3d0092af97ae94b81dcd0afad86804ed90ba8
lnardon <lucaslnardon@gmail.com>
2023-11-02 19:44:10 -0300

    :sparkles: Update package and demo

--- index.ts
+++ index.ts
@@ -1,11 +1,27 @@
 function animateText(text:string, target:string){
     const targetElement = document.getElementById(target);
-
+    document.styleSheets[0].insertRule(appear)
     const container = document.createElement("div")
-    text.split("").forEach(letter => {
+    text.split("").forEach((letter,index) => {
         const letterEl = document.createElement("span");
         letterEl.innerText = letter;
-        container.appendChild(letterEl)
+        letterEl.style.animation = "appear 1s ease forwads"
+        letterEl.style.animationDelay = (35 * index) + "ms";
+        container.appendChild(letterEl);
     });
     targetElement.appendChild(container)
 }
+
+const appear = `
+@keyframes appear {
+    from {
+        opacity: 0;
+        transform: scale(0.7)
+    }
+
+    to {
+        opacity: 1;
+        transform: scale(1)
+    }
+}
+`
